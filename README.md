314c5add4e657d3cf23a70aa84208dfc5856dc68
jdu211171 <211171m@jdu.uz>
2024-01-02 19:40:59 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # express-template
-# express-template
---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+const port = 3000;
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("HELLO FROM EXPRESS + TS!!!!");
+});
+
+app.get("/hi", (req: Request, res: Response) => {
+  res.send("BYEEE!!");
+});
+
+app.listen(port, () => {
+  console.log(`now listening on port ${port}`);
+});
+
