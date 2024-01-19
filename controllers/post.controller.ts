bcf764335bd4bbf708cec296547df673457961e6
jdu211171 <211171m@jdu.uz>
2024-01-19 08:19:35 +0500

    Post pagination added

--- controllers/post.controller.ts
+++ controllers/post.controller.ts
@@ -4,11 +4,13 @@ import PostRepository from "../models/post.repository";
 const router = express.Router();
 
 router.get('/all', async (req, res) => {
+    const currentLoad = Number(req.query.currentLoad) || 1;
+    const limit = Number(req.query.limit) || 10;
     try {
-        const posts = await PostRepository.allPosts();
+        const posts = await PostRepository.allPosts(currentLoad, limit);
         return res.status(200).json(posts).end();
     } catch (e: any) {
-        return res.status(500).json({ message: e.message }).end();
+        return res.status(500).json({message: e.message}).end();
     }
 });
 
@@ -16,11 +18,11 @@ router.get('/find/:id', async (req, res) => {
     try {
         const post = await PostRepository.findPost(Number(req.params.id));
         if (!post) {
-            return res.status(404).json({ message: 'Post not found' }).end();
+            return res.status(404).json({message: 'Post not found'}).end();
         }
         return res.status(200).json(post).end();
     } catch (e: any) {
-        return res.status(500).json({ message: e.message }).end();
+        return res.status(500).json({message: e.message}).end();
     }
 });
 
@@ -29,7 +31,7 @@ router.post('/create', async (req, res) => {
         const newPost = await PostRepository.createPost(Number(req.body.user.id), req.body.content);
         return res.status(200).json(newPost).end();
     } catch (e: any) {
-        return res.status(500).json({ message: e.message }).end();
+        return res.status(500).json({message: e.message}).end();
     }
 });
 
@@ -38,7 +40,7 @@ router.put('/update/:id', async (req, res) => {
         const updatedPost = await PostRepository.updatePost(Number(req.params.id), req.body.content);
         return res.status(200).json(updatedPost).end();
     } catch (e: any) {
-        return res.status(500).json({ message: e.message }).end();
+        return res.status(500).json({message: e.message}).end();
     }
 });
 
@@ -47,7 +49,7 @@ router.delete('/delete/:id', async (req, res) => {
         const deletedPost = await PostRepository.deletePost(Number(req.params.id));
         return res.status(200).json(deletedPost).end();
     } catch (e: any) {
-        return res.status(500).json({ message: e.message }).end();
+        return res.status(500).json({message: e.message}).end();
     }
 });
 
